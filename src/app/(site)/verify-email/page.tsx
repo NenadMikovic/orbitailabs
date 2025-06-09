ea460716810f8c6d2d473e34bfb49b659378841e
Nenad Mikovic <nenad.mikovic@outlook.com>
2025-06-09 21:04:57 +0200

    verify email page

--- src/app/(site)/verify-email/page.tsx
+++ src/app/(site)/verify-email/page.tsx
@@ -1,6 +1,6 @@
 import VerifyEmailClient from "@/components/VerifyEmailClient";
 
 export default function VerifyEmailPage() {
-  return 
-  <VerifyEmailClient />;
+  return <VerifyEmailClient />;
 }
+
