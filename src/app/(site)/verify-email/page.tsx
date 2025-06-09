9691f299da7962f21ab48b30b37c8b1b013193e9
Nenad Mikovic <nenad.mikovic@outlook.com>
2025-06-10 00:17:25 +0200

    new verification route

--- src/app/(site)/verify-email/page.tsx
+++ src/app/(site)/verify-email/page.tsx
@@ -1,6 +1,12 @@
+'use client';
+
+import { Suspense } from "react";
 import VerifyEmailClient from "@/components/VerifyEmailClient";
 
 export default function VerifyEmailPage() {
-  return 
-  <VerifyEmailClient />;
+  return (
+    <Suspense fallback={<div className="text-white text-center pt-10">Verifying email...</div>}>
+      <VerifyEmailClient />
+    </Suspense>
+  );
 }
